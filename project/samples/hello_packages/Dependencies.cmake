93449e705e1082740cc63cf1d978059f84df585a
01Pollux <0139ne@gmail.com>
2025-07-21 23:57:07 +0100

    feat: add LinkDependencies module and update dependency loading function

--- project/samples/hello_packages/Dependencies.cmake
+++ project/samples/hello_packages/Dependencies.cmake
@@ -5,11 +5,12 @@
 #     used for logging
 #
 function(target_load_dependencies target)
-	target_add_dependency(${target}
-		PACKAGES
-			spdlog
-				URL			https://github.com/gabime/spdlog/archive/refs/tags/v1.15.2.zip
-				URL_HASH	SHA256=d91ab0e16964cedb826e65ba1bed5ed4851d15c7b9453609a52056a94068c020
-				OPTIONS		"SPDLOG_BUILD_SHARED ON" 
+	CPMAddPackage(${target}
+		NAME		spdlog
+		URL			https://github.com/gabime/spdlog/archive/refs/tags/v1.15.2.zip
+		URL_HASH	SHA256=d91ab0e16964cedb826e65ba1bed5ed4851d15c7b9453609a52056a94068c020
+		OPTIONS		"SPDLOG_BUILD_SHARED ON" 
 	)
+
+	target_link_dependencies_auto(${target} PRIVATE spdlog::spdlog)
 endfunction()
