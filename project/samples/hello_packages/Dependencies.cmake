69dfeec6cf11ad1fd0cb1f9ad6d106a6a94143da
01Pollux <0139ne@gmail.com>
2025-07-28 09:16:04 +0100

    refactor: clean up CMakeLists and update dependency loading for HelloPackages

--- project/samples/hello_packages/Dependencies.cmake
+++ project/samples/hello_packages/Dependencies.cmake
@@ -1,16 +1,19 @@
-#
-# loads dependencies for the target
+# Dependencies for the target
 # adds:
 #   - spdlog:
 #     used for logging
 #
-function(target_load_dependencies target)
-	CPMAddPackage(${target}
+function(target_load_dependencies TARGET_NAME)
+	CPMAddPackage(
 		NAME		spdlog
 		URL			https://github.com/gabime/spdlog/archive/refs/tags/v1.15.2.zip
 		URL_HASH	SHA256=d91ab0e16964cedb826e65ba1bed5ed4851d15c7b9453609a52056a94068c020
-		OPTIONS		"SPDLOG_BUILD_SHARED ON" 
+		OPTIONS		"SPDLOG_BUILD_SHARED OFF" "SPDLOG_FMT_EXTERNAL OFF"
+		SYSTEM		TRUE
 	)
 
-	target_link_dependencies_auto(${target} PRIVATE spdlog::spdlog)
+	target_link_dependencies_auto(${TARGET_NAME}
+		PRIVATE
+			spdlog::spdlog
+	)
 endfunction()
